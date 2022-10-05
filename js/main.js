867be274c4606e8d43d2e8db56413107ca3fcecf
mxz22345 <105110779+mxz22345@users.noreply.github.com>
2022-10-05 16:16:15 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive (min, max) {
+  if (min < 0 || 0 > max) {
+    return NaN
+  }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getRandomIntInclusive(1, 9))
+window.console.log (getRandomIntInclusive(1, 9))
 
-//подсмотрел в дискорде сам до конца не особо понимаю как тут все работает
 
-function checkMaxStringLength(verifiedString, minLength, maxLength) {
+function checkMaxStringLength (string, lenght) {
 
-  const userInput = verifiedString.value;
-  if (verifiedString > 0 && userInput.length >= minLength && userInput.length <= maxLength) {
-    return true;
-  } else {
-    return false;
-  }
+  return string.lenght <= lenght;
 }
 
-checkMaxStringLength (20,20,140);
-
-
+checkMaxStringLength ("",140);
