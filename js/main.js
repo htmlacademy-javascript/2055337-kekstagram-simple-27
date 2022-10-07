dd03208b3b703efe9747a656d8b4d92b39beb320
mxz22345 <105110779+mxz22345@users.noreply.github.com>
2022-10-07 17:17:07 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,18 @@
 
 function getRandomIntInclusive (min, max) {
   if (min < 0 || 0 > max) {
-    return NaN
+    return NaN;
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-window.console.log (getRandomIntInclusive(1, 9))
-
+window.console.log (getRandomIntInclusive(1, 9));
 
 function checkMaxStringLength (string, length) {
 
   return string.length <= length;
 }
 
-checkMaxStringLength ("",140);
+checkMaxStringLength ('',140);
