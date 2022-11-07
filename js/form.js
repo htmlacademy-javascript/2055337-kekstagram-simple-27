1383acf9530ad1d5017be8df4f49e7d8dd2e131b
mxz22345 <105110779+mxz22345@users.noreply.github.com>
2022-11-07 12:12:17 +0500

    добавление effect.js

--- js/form.js
+++ js/form.js
@@ -2,6 +2,10 @@ import {
   resetScale
 } from './scale.js';
 
+import {
+  resetEffects
+} from './effect.js';
+
 const form = document.querySelector('.img-upload__form');
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('body');
@@ -18,6 +22,7 @@ const showModal = () => {
 const hideModal = () => {
   form.reset();
   resetScale();
+  resetEffects();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscKeyDown);
