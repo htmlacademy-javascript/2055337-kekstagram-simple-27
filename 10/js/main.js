6e154b3ef054f5face5fa32cbb0e4ac796c11ef8
Keks <keks@htmlacademy.ru>
2022-11-17 01:11:19 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,16 @@
+
+import {
+  renderPictures
+} from './picture.js';
+
+import './effect.js';
+
+import './scale.js';
+
+import './form.js';
+
+import {
+  getData
+}from './api.js';
+
+getData(renderPictures);
